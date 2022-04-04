2e9dc0c9e39ddb2205df0d6f4735d06e2ef04245
Ing.Juan Gonzalez <pashitox@gmail.com>
2022-04-04 09:33:15 +0200

    avances

--- src/App.js
+++ src/App.js
@@ -3,44 +3,70 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { email:'', pass:''};
- 
- 
+    this.state = { inputs:""};
+   
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  };
 
-  handleChange(evt) {
-    this.setState({ [evt.target.name]: evt.target.value });
-  }
+   handleChange(evt) {
+    this.setState({[evt.target.name]: evt.target.value });
+   
+  };
 
-  handleSubmit(evt) {
-      
+ 
+
+  handleSubmit = (event) => {
+ 
+      const datos = {
+        name: this.state.name,
+        pwd: this.state.pwd
+      };
+     
+     event.preventDefault();
+     console.log(datos);
 
 
-    alert(`The email ${this.state.email} you entered the password was: ${this.state.pass}`);
-    evt.preventDefault();
-    this.setState({email: "", pass:"" });
-  }
+  };
   
 
+
+  //function Saludo(props) {
+    //const isLoggedIn = props.isLoggedIn;
+  //  if (isLoggedIn) {
+    //  return <Bienvenida />;
+   // }
+   // return <UsuarioAnonimo />;
+ // }
+     
+        //alert(`The User ${this.state.UserName} you entered the password was: ${this.state.pass}`);
+      //evt.preventDefault();
+      //this.setState({UserName: "", pass:"" });
+    
   render() {
+          
+    const { name, pwd } = this.state;
+       
     return (
-    <form onSubmit={this.handleSubmit}>
-    <label >
-
-      Email:
-      <input type="email" 
-      name="email" email={this.state.email} 
-      onChange={this.handleChange} />
-      Password:
-
-      <input type="password"
-      name="pass" pass={this.state.pass}
-      onChange={this.handleChange} />
-    </label>
-    <input type="submit" value="Submit" />
-      </form>
+      <form onSubmit={this.handleSubmit}>
+      <label>Enter your name:
+      <input 
+        type="text" 
+        name="name" 
+        value={name || ''} 
+        onChange={this.handleChange}
+      />
+      </label>
+      <label>Enter your password:
+        <input 
+          type="password" 
+          name="pwd" 
+          value={pwd || ''} 
+          onChange={this.handleChange}
+        />
+        </label>
+        <input type="submit" />
+    </form>
 
   );
 } }
