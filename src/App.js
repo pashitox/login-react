21496c98a22021e34f8d51a87f66e58516bb6b77
Ing.Juan Gonzalez <pashitox@gmail.com>
2022-04-12 20:41:02 +0200

    botto disable

--- src/App.js
+++ src/App.js
@@ -5,100 +5,118 @@ class App extends React.Component {
     super(props);
     this.state = {
       name:"",
-      pwd:"",
-      errors:[],
-   
+      envio: false,
+      errors:["Your password must contain at least one digit.", "el pepe."]  
   };
    
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.validation = this.validation.bind(this);
+   this.handleDelete = this.handleDelete.bind(this);
   };
 
    handleChange(evt) {
-
-
     this.setState({[evt.target.name]: evt.target.value });
-
   };
 
   
+  handleDelete (index) {
+  const errors = this.state.errors
+  errors.splice(index, 1); 
+ // const del = errors.filter(error => error.id !== error);
+   console.log(errors);
+  this.setState({ errors:errors });
+  }
 
-  handleSubmit = (event) => {    
-     
-    event.preventDefault()
-        const name  = this.state.name;
-       // const errors = this.state.errors;
-
-        const  val = this.validation(name);
-
-       console.log(val)
-       this.setState({name:""});
-        
-      //  if (name.length < 4) {
-       
-        //  errors.push("randon");
-        
-    //  this.setState({ errors:[]});
-        
+  handleSubmit = (e) => {      
+   const name  = this.state.name;        
+   // const errors = this.state.errors;
+    const val = this.validation(name);
+    console.log(val);    
+ //  if (name.length < 4) {
+//  errors.push("randon");
+//  this.setState({ errors:[]});
+      this.setState({name:""});
+       e.preventDefault() 
         };
            
   
   
-  validation = (name) =>{
-    
-    console.log(name);
-
-    if (name.length < 4) {      
-      const errors = this.state.errors; 
-      const valor = errors.includes("please, tu password must be mayor 4 digits")
-     console.log(valor);      
+  validation = (e) => {    
+    console.log(e);
+      
+    const anch = e.length
+    console.log (anch);    
+   if (anch < 4) {      
+   const errors = this.state.errors;       
+   const valor = errors.includes("please, tu password must be mayor 4 digits");         
+   console.log(valor);
+      
      if (valor === false)  {
-        alert("qwe")
-         errors.push("please, tu password must be mayor 4 digits"); 
-        }   
-       
-  }
+     const errors = this.state.errors; 
+     alert("qwe")
+      errors.push("please, tu password must be mayor 4 digits"); 
+     }
+ 
+ //else {
+   //   const errors = this.state.errors;          
+   //   const index = errors.indexOf("please, tu password must be mayor 4 digits");    
+   //   console.log(index);
+   //   errors.splice(index, 1); 
+//
+   // }    
+    } if (e.search(/^[a-zA-Z ]/i) < 0) {
+         const errors = this.state.errors; 
+              const valor = errors.includes("Your password must contain at least one letter.")
+       if (valor === false)  {
+        errors.push("Your password must contain at least one letter.");
+      }
+      }
+      if (e.search(/[0-9]/i) < 0) {
+      const errors = this.state.errors;       
+      const valor = errors.includes("Your password must contain at least one digit.")
+      if (valor === false)  {
+      errors.push("Your password must contain at least one digit.");
+      }
+ 
+ 
+ 
+       //};
   
 
  // return 1
-  }
+  };
  // this.setState({errors: errors});
 
-   
+  }
 
 
   render() {
-          
-    const { name, errors} = this.state;
-   
+ 
+    const errors = this.state.errors;
+    const listerros = errors.map((errors,i) =>
+      <li key={i}>{errors}
+      <button onClick={()=>this.handleDelete(i)}>x</button>
+      </li>
+      
+    );
             
     return (
-      <form onSubmit={this.handleSubmit}>
+   <div>   
+     <form onSubmit={this.handleSubmit}>
       <label>Enter your name:
         <input
         id="name"
         type="text" 
         name="name" 
-        value={name}
-       
-       onChange={this.handleChange} /> 
-      </label>
-
-      <div>{errors.map(error => <p key={error}>{error}</p>)}</div>       
- 
-
-      <label>Enter your password:
-        <input 
-        //  type="password" 
-         // name="pwd" 
-         // value={pwd || ''} 
-          onChange={this.handleChange}          
-        />
-        </label>
-        <div>{this.state.error}</div>
-        <input type="submit" />
+        value={this.state.name}       
+        onChange={this.handleChange} /> 
+      </label>     
+      <button disabled={!this.state.name} type="submit">submit</button>
     </form>
+    <ul>{listerros}</ul>
+    </div>
+    
 
   );
 } }
